5e27a2a87e314f3084c6e5365d6cf593a2f6a3b4
dan branton <daniel.branton@uptimesoftware.com>
2014-08-23 15:31:02 -0400

    moved all the Gadget’s JS out of the definition.xml and into it’s own File

---
+++ src/AllServicesGadget.js
@@ -0,0 +1,55 @@
+        function editSettings(settings) {
+
+            $("#AllServicesView").hide();
+            $("#edit").show();
+
+
+        }
+
+        function doError(status) {
+            $("#error").empty();
+            uptimeErrorFormatter.getErrorBox(status, "Error communicating with up.time").appendTo('#error');
+            $("#error").slideDown();
+            $("#save").prop("disabled", false);
+        }
+
+        function doRender(settings) {
+        $("#edit").hide();
+        $("#AllServicesView").show();
+                var serviceStatusTable = new UPTIME.ServiceStatusTable({
+                    id : "allServicesList",
+                    ajaxParams : "&queryName=GET_STATUS",
+                    timeout : 60000,
+                    alternateAjaxParams : "&queryName=GET_STATUS&queryParameter=hideOk&queryValue=true"             });
+                serviceStatusTable.start();
+        }
+
+        $("#save").click(function() {
+            var settings = {
+                test: "test"
+            };
+
+            uptimeGadget.saveSettings(settings).then(doRender, doError);
+
+        });
+
+
+
+        uptimeGadget.registerOnResizeHandler(function(dimensions) {
+            $("body").height(dimensions.height).width(dimensions.width);
+        });
+        uptimeGadget.registerOnEditHandler(function() {
+            $("#edit").hide();
+            $("#AllServicesView").show()
+            uptimeGadget.loadSettings().then(function(settings) {
+                editSettings(settings);
+            });
+        });
+        uptimeGadget.registerOnLoadHandler(function(onLoadData) {
+            $("#AllServicesView").data("dimensions", onLoadData.dimensions);
+            if (onLoadData.hasPreloadedSettings()) {
+                doRender(onLoadData.settings);
+            } else {
+                uptimeGadget.loadSettings(doRender, doError);
+            }
+        });
